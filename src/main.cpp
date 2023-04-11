bf8dc53fbeca0392363d1b7e0f515cf68171d3fe
Oren Leiman <>
2023-04-10 22:27:50 -0700

    ray.hpp
    Linearly blend white and blue depending on the y coordinate.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
 
 #include "color.hpp"
+#include "ray.hpp"
 #include "vec3.hpp"
 
 #include <chrono>
 #include <iostream>
 #include <thread>
 
+color ray_color(const ray &r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  auto t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main() {
 
   // Image
 
-  constexpr int image_width = 256;
-  constexpr int image_height = 256;
+  constexpr auto aspect_ratio = 16.0 / 9.0;
+  constexpr int image_width = 400;
+  constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
+
+  // Camera
+
+  auto viewport_height = 2.0;
+  auto viewport_width = aspect_ratio * viewport_height;
+  auto focal_length = 1.0;
+
+  auto origin = point3(0, 0, 0);
+  auto horizontal = vec3(viewport_width, 0, 0);
+  auto vertical = vec3(0, viewport_height, 0);
+  auto lower_left_corner =
+      origin - (horizontal / 2) - (vertical / 2) - vec3(0, 0, focal_length);
 
   // Render
 
@@ -20,9 +40,11 @@ int main() {
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      color pixel_color(double(i) / (image_width - 1),
-                        double(j) / (image_height - 1), .25);
-
+      auto u = double(i) / (image_width - 1);
+      auto v = double(j) / (image_height - 1);
+      ray r(origin,
+            lower_left_corner + (u * horizontal) + (v * vertical) - origin);
+      color pixel_color = ray_color(r);
       write_color(std::cout, pixel_color);
     }
   }
