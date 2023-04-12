63087cff09ce46565dfbbf41ba455fc183493c8c
Oren Leiman <>
2023-04-11 19:24:43 -0700

    Basic diffuse material with a few diffusion strategies

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,17 @@
 
 #include <iostream>
 
-color ray_color(const ray &r, const hittable &world) {
+color ray_color(const ray &r, const hittable &world, int depth) {
   hit_record rec;
-  if (world.hit(r, 0, infinity, rec)) {
-    return 0.5 * (rec.normal + color(1, 1, 1));
+  if (depth <= 0) {
+    return color(0, 0, 0);
+  }
+
+  if (world.hit(r, 0.001, infinity, rec)) {
+    // point3 target = rec.p + rec.normal + random_in_unit_sphere();
+    // point3 target = rec.p + rec.normal + random_unit_vector();
+    point3 target = rec.p + random_in_hemisphere(rec.normal);
+    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
   }
 
   vec3 unit_direction = unit_vector(r.direction());
@@ -25,7 +32,8 @@ int main() {
   constexpr auto aspect_ratio = 16.0 / 9.0;
   constexpr int image_width = 400;
   constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
-  const int samples_per_pixel = 100;
+  constexpr int samples_per_pixel = 100;
+  constexpr int max_depth = 50;
 
   // World
 
@@ -49,7 +57,7 @@ int main() {
         auto u = (i + random_double()) / (image_width - 1);
         auto v = (j + random_double()) / (image_height - 1);
         ray r = cam.get_ray(u, v);
-        pixel_color += ray_color(r, world);
+        pixel_color += ray_color(r, world, max_depth);
       }
       write_color(std::cout, pixel_color, samples_per_pixel);
     }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -5,6 +5,19 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <random>
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+  // return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+  // Returns a random real in [min,max).
+  return min + (max - min) * random_double();
+}
 
 class vec3 {
 public:
@@ -45,6 +58,15 @@ public:
 
   double length_squared() const {
     return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+  }
+
+  inline static vec3 random() {
+    return vec3(random_double(), random_double(), random_double());
+  }
+
+  inline static vec3 random(double min, double max) {
+    return vec3(random_double(min, max), random_double(min, max),
+                random_double(min, max));
   }
 
 private:
@@ -91,3 +113,26 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 }
 
 inline vec3 unit_vector(vec3 v) { return v / v.length(); }
+
+inline vec3 random_in_unit_sphere() {
+  while (true) {
+    auto p = vec3::random(-1, 1);
+    if (p.length_squared() >= 1)
+      continue;
+    return p;
+  }
+}
+
+inline vec3 random_unit_vector() {
+  return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3 &normal) {
+  vec3 in_unit_sphere = random_in_unit_sphere();
+  if (dot(in_unit_sphere, normal) >
+      0.0) { // In the same hemisphere as the normal
+    return in_unit_sphere;
+  } else {
+    return -in_unit_sphere;
+  }
+}
