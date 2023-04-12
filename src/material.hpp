50d0b755ba2bcb1f3a0cde1956336bb48b55e71c
Oren Leiman <>
2023-04-11 22:06:32 -0700

    Glass
    - dielectric material
    - various tricks for refractance/reflectance

--- src/material.hpp
+++ src/material.hpp
@@ -35,3 +35,22 @@ private:
   color albedo;
   double fuzz;
 };
+
+class dielectric : public material {
+public:
+  dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+  virtual ~dielectric() = default;
+
+  virtual bool scatter(const ray &r_in, const hit_record &rec,
+                       color &attenuation, ray &scattered) const override;
+
+private:
+  double ir;
+
+  static double reflectance(double cosine, double ref_idx) {
+    // Use Schlick's approximation for reflectance
+    auto r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+  }
+};
